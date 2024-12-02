e722578f725f26fcae037cc8fcd1f951b1340470
Alexander Frischbutter <alexander@noah.tech>
2024-12-02 09:41:44 +0100

    fixed running

--- odoo-docker/entrypoint.sh
+++ odoo-docker/entrypoint.sh
@@ -6,14 +6,11 @@ cat << EOF > /etc/odoo/odoo.conf
 addons_path = ${ADDONS_PATH}
 data_dir = ${DATA_DIR}
 admin_passwd = ${ADMIN_PASSWORD}
-
-# Database settings
 db_host = ${DB_HOST}
 db_port = ${DB_PORT}
 db_user = ${DB_USER}
 db_password = ${DB_PASSWORD}
-db_name = ${DB_NAME}
+db_name = odoo
 EOF
 
-# Execute CMD
 exec "$@"
